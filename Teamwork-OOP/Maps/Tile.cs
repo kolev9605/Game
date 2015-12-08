97e06c3031a27bc5720f63437d76ffe800f7a574
sideroff <ivan.sideroff@gmail.com>
2015-12-08 11:18:19 +0200

    doing shit

---
+++ Teamwork-OOP/Maps/Tile.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Teamwork_OOP.Maps
+{
+    public abstract class Tile
+    {
+        private const int width = 50;
+        private const int height = 50;
+        private readonly string path;
+
+        public string Path { get;}
+
+        //TODO: Implement effects on characters
+
+
+        protected Tile(string path)
+        {
+            this.Texture2D = this.conte
+        }
+
+        public Texture2D Texture2D { get; set; }
+    }
+}
